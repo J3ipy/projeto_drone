b4d02b9342d4cfb3aacb9168b454190851d3d35e
João Pedro <97753966+J3ipy@users.noreply.github.com>
2025-05-07 18:07:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,27 @@ from djitellopy import Tello
 
 # Posição dos waypoints [x, y, z] em metros
 path = np.array([
-    [0,   0,  -0.4],
-    [0,   0,  -0.4],
-    [0,   0,  -0.4],
-    [0,   0.6,  0],
-    [0,   0,   0.4],
-    [0,   0,   0.4],
-    [0,  -0.6,  0.4]
+    [0,   0,  0.5], #subir OK
+
+    [0,  -0.7,  0.5], # primeira diagonal direita OK
+    [0,  -0.7,  0.5], # primeira diagonal direita OK
+
+    [0,  -0.7,  -0.5], # segunda diagonal direita OK 
+    [0,  -0.7,  -0.5], # segunda diagonal direira OK
+
+    [0,  0.7,  0.5], # volta da segunda diagonal direita OK
+    [0, 0.7,  0.5], # volta da segunda diagonal direita OK
+
+    [0,  0.7,  -0.5], # volta da primeira diagonal direita OK
+    [0,  0.7,  -0.5], # volta da primeira diagonal direita OK
+
+    [0,   0,  -0.5], #pousar OK
 ])
 
 # Conectar e entrar em modo SDK
 tello = Tello()
 tello.connect()
-print(f"Bateria inicial: {tello.get_battery()}%")
+print(f"Bateria inicial: {tello.get_battery()}%") # verifica a bateria atual
 
 # Decolar
 tello.takeoff()
@@ -24,10 +32,9 @@ time.sleep(2)
 
 # Seguir o path
 for idx, waypoint in enumerate(path, start=1):
-    # converter para centímetros
-    dx, dy, dz = (waypoint * 100).astype(int)
-    print(f"Waypoint {idx}: go {dx} {dy} {dz} at speed 20")
-    # Comando 'go x y z speed' do SDK 2.0 :contentReference[oaicite:0]{index=0}:contentReference[oaicite:1]{index=1}
+    # converter para centímetros e garantir tipo int
+    dx, dy, dz = map(int, waypoint * 100) # converte metros em cm (O SDK do tello trabalha em cm)
+    print(f"Waypoint {idx}: go {dx} {dy} {dz} at speed 20") # move em direção relativa e com uma velocidade de 20 cm/s
     tello.go_xyz_speed(dx, dy, dz, 20)
     time.sleep(2)  # pausa para estabilizar
 
